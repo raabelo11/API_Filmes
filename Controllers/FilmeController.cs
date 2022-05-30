23bba95499868e7821927664a9d91b75a4a62c2e
Guilherme Lima <guilhermepainn@hotmail.com>
2022-05-30 19:09:18 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmesAPI.Models;
+using FilmesAPI.Data;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost] //Criar recurso novo.
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            _context.Filmes.Add(filme);
+            _context.SaveChanges(); // salva o json no banco de dados
+            return CreatedAtAction(nameof(RetornaFilmesPorId), new { Id = filme.Id }, filme); //informa aonde o json foi gravado local onde pode ser acessado
+            
+        }
+
+        [HttpGet] //Retorna os dados do filme.
+        public IEnumerable<Filme> RetornaFilmes()
+        {
+            return(_context.Filmes);
+        }
+
+        [HttpGet("{id}")] // identificador do ID do filme desejado a ser retornado: ex: localhost:500/filme/1
+        public IActionResult RetornaFilmesPorId(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);  // retorna null quando não existe filme com ID indicado.
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 250, ErrorMessage = "Duração deve conter no mínimo 1 e no máximo 300 minutos")]
+        public int Duracao { get; set; }
+    }
+}
