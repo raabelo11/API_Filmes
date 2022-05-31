eb3ab7eb3ddfb12823a02996c624f0c8c910996f
Guilherme Lima <guilhermepainn@hotmail.com>
2022-05-30 23:19:41 -0300

    Inclusão do Put e Delete, criação de novas classes DTO para enviar dados do filme sem precisar enviar ID e inclusão da DateTime Now na consulta de filmes por ID

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using FilmesAPI.Models;
 using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
 
 namespace FilmesAPI.Controllers
 {
@@ -19,30 +20,85 @@ namespace FilmesAPI.Controllers
             _context = context;
         }
 
-        [HttpPost] //Criar recurso novo.
-        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        [HttpPost("Adiciona_filme")] //Criar recurso novo.
+        public IActionResult AdicionaFilme([FromBody] AdicionaFilmesDto adFilmesDto)
         {
+            Filme filme = new Filme
+            {
+                Titulo = adFilmesDto.Titulo,
+                Genero = adFilmesDto.Genero,
+                Duracao = adFilmesDto.Duracao,
+                Diretor = adFilmesDto.Diretor,
+            };
+
             _context.Filmes.Add(filme);
             _context.SaveChanges(); // salva o json no banco de dados
             return CreatedAtAction(nameof(RetornaFilmesPorId), new { Id = filme.Id }, filme); //informa aonde o json foi gravado local onde pode ser acessado
             
         }
 
-        [HttpGet] //Retorna os dados do filme.
+        [HttpGet("Retorna_filme")] //Retorna os dados do filme.
         public IEnumerable<Filme> RetornaFilmes()
         {
             return(_context.Filmes);
         }
 
-        [HttpGet("{id}")] // identificador do ID do filme desejado a ser retornado: ex: localhost:500/filme/1
+        [HttpGet("Retorna_filmes {id}")] // identificador do ID do filme desejado a ser retornado: ex: localhost:500/filme/1
         public IActionResult RetornaFilmesPorId(int id)
         {
-            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);  // retorna null quando não existe filme com ID indicado.
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);  //recupera o filme já cadastrado, se for null = NotFound
             if(filme != null)
             {
-                return Ok(filme);
+                LerFilmesDto lerfilmesDto = new LerFilmesDto
+                {
+                    Titulo = filme.Titulo,
+                    Diretor = filme.Diretor,
+                    Duracao = filme.Duracao,
+                    Id = filme.Id,
+                    Genero = filme.Genero,
+                    HoraDaConsulta = DateTime.Now
+                };
+                return Ok(lerfilmesDto);
             }
             return NotFound();
+
+        }
+
+
+        [HttpPut("Modifica_filme {id}")]
+        public IActionResult ModificarFilme(int id, [FromBody] ModificaFilmesDto modFilmesDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                filme.Titulo = modFilmesDto.Titulo;
+                filme.Genero = modFilmesDto.Genero;
+                filme.Diretor = modFilmesDto.Diretor;
+                filme.Duracao = modFilmesDto.Duracao;
+                _context.SaveChanges();
+                return NoContent();
+            }
+                
+        }
+
+        [HttpDelete("Deleta_filme")]
+        public IActionResult DeletaFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id); //recupera o filme já cadastrado, se for null = NotFound
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Remove(filme);
+                _context.SaveChanges();
+                return NoContent();
+            }
         }
 
     }
---
+++ Data/Dtos/ModificaFilmesDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ModificaFilmesDto
+    {
+        [Required(ErrorMessage = "Campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 250, ErrorMessage = "Duração deve conter no mínimo 1 e no máximo 300 minutos")]
+        public int Duracao { get; set; }
+    }
+}
